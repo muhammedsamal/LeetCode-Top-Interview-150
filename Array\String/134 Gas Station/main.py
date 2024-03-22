c2113290236112bd64aef0dec642034d8d4a0ad7
Muhammed Samal <muhammedsamalt@gmail.com>
2024-03-22 11:01:44 +0000

    added gas station

---
+++ Array\String/134 Gas Station/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        total, start = 0, 0
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+
+            if total < 0:
+                total = 0
+                start = i + 1
+        return start   
