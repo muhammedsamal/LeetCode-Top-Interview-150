a04b4a5dfc9122f88ec4cb32966eaa127ed2f871
Muhammed Samal <muhammedsamalt@gmail.com>
2024-03-31 11:46:10 +0100

    added two sum II

---
+++ Two Pointers/67 Two Sum II - Input Array Is Sorted/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+
+        while l < r:
+            currSum = numbers[l] + numbers[r]
+            if currSum > target:
+                r -= 1
+            elif currSum < target:
+                l += 1
+            else:
+                return [l+1, r+1]
